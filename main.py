2771ab31b74fa9e64abfc3cf7d08a93a857b2d30
Jess-chege <biankachege98@gmail.com>
2021-09-26 21:51:37 +0300

    push change

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 def predict():
     # get data
-    data = request.get_json(force=True)
+    data = request.get_json(false=True)
 
     # convert data into dataframe
     data.update((x, [y]) for x, y in data.items())
